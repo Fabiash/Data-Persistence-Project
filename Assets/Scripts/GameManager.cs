9bc1d7da95db0f08c4f5250a3907dc724af3c9ad
Fabiash <97519330+Fabiash@users.noreply.github.com>
2022-01-26 18:48:35 +0100

    26/01/2022 comiit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private MainManager mainManager;
+    private int bestScore;
+    public string currentPlayerName;
+    public Brick BrickPrefab;
+    public int LineCount = 6;
+    public Rigidbody Ball;
+    public Text BestScoreText;
+
+    public Text ScoreText;
+    public GameObject GameOverText;
+
+    private bool m_Started = false;
+    public int m_Points;
+
+    private bool bestScoreInThisGame = false;
+
+    private bool m_GameOver = false;
+
+    void Start()
+    {
+        mainManager = GameObject.Find("Main Manager").GetComponent<MainManager>();
+        bestScore = mainManager.bestScore;
+        currentPlayerName = mainManager.playerName;
+
+        if (mainManager.bestScoreSet)
+        {
+            BestScoreText.text = "Best Score: " + mainManager.bestScoreName + ": " + bestScore;
+        }
+
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!m_Started)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_Started = true;
+                float randomDirection = Random.Range(-1.0f, 1.0f);
+                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                forceDir.Normalize();
+
+                Ball.transform.SetParent(null);
+                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+            }
+        }
+        else if (m_GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    void AddPoint(int point)
+    {
+        m_Points += point;
+        ScoreText.text = "Score: " + m_Points;
+        if (m_Points > bestScore)
+        {
+            if (!bestScoreInThisGame)
+            {
+                bestScoreInThisGame = true;
+            }
+            BestScoreText.text = "<color=red>Best Score: " + currentPlayerName + ": " + m_Points + "</color>";
+        }
+    }
+
+    public void GameOver()
+    {
+        
+        if (bestScoreInThisGame)
+        {
+            mainManager.SetBestScore();
+        }
+        m_GameOver = true;
+        GameOverText.SetActive(true);
+    }
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,71 +6,50 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+    public static MainManager Instance;
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    [SerializeField] GameObject placeholder;
+    [SerializeField] GameObject playerNameField;
+    public string playerName;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    private GameManager gameManager;
+    public int bestScore;
+    public string bestScoreName;
+    public bool bestScoreSet;
+
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        if (playerNameField.GetComponent<Text>().text == "")
+        {
+            placeholder.GetComponent<Text>().color = Color.red;
+        }
+        else
+        {
+            playerName = playerNameField.GetComponent<Text>().text;
+            SceneManager.LoadScene(1);
+        }
         
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
     }
 
-    private void Update()
+    public void SetBestScore()
     {
-        if (!m_Started)
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        if (!bestScoreSet)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
+            bestScoreSet = true;
         }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
-
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
-    }
-
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+        bestScore = gameManager.m_Points;
+        bestScoreName = gameManager.currentPlayerName;
     }
 }
